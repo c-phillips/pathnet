9d68a0fd52c11f4471f85a88d08ae30c20a35721
c-phillips <collinp1024@gmail.com>
2019-02-02 15:07:38 -0500

    Confirmed training. The accuracy metric is just not correct? Also, tensorboard does not show ops

--- pathnet.py
+++ pathnet.py
@@ -241,3 +241,34 @@ class Pathnet:
                 print(f"\nValidation: L({loss:.6f}), A({acc*100:.4f}%)")
             print("\nFinished!")
             writer.close()
+
+            rows = 6
+            columns = 5
+            images = rows*columns
+            num_correct = 0
+            plt.figure()
+            for i in range(images):
+                y = sess.run(self.output, feed_dict={self.data_layer.x_input:np.expand_dims(x_test[i],axis=0), self.data_layer.y_input:np.expand_dims(y_test[i], axis=0), self.Pmat:path})
+                # print(y[0])
+                # print(y_test[i])
+                # print("")
+
+                class_names = [ 'T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 
+                                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+                cat_nums = np.arange(len(y[0]))
+
+                imax = plt.subplot(rows, 2*columns, 2*i+1)
+                imax.imshow(x_test[i].reshape(28,28))
+
+                predax = plt.subplot(rows, 2*columns, 2*i+2)
+                correct = False
+                if np.argmax(y[0]) == np.argmax(y_test[i]):
+                    correct = True
+                    num_correct += 1
+                predax.bar(cat_nums, y[0], color='g' if correct else 'r')
+                predax.set_xticks(cat_nums)
+                predax.set_xticklabels(class_names, rotation=60)
+
+            print(f"Example accuracy: {num_correct/images*100}%")
+            plt.subplots_adjust(hspace=0.4)
+            plt.show()
